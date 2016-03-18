92e37dcedd0bfd6dcc53bdaec2156d72eb9d56eb
Maurice Bos <m-ou.se@m-ou.se>
2016-03-18 14:08:16 +0100

    Fix year in copyright notice.

--- parse.py
+++ parse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2015 Nuxi, https://nuxi.nl/
+# Copyright (c) 2016 Nuxi, https://nuxi.nl/
 #
 # This file is distributed under a 2-clause BSD license.
 # See the LICENSE file for details.
