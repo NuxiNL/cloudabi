37e1914979d9a988e746547da436e1c21f81e8ea
Maurice Bos <m-ou.se@m-ou.se>
2016-03-19 16:34:47 +0100

    Add test script.

---
+++ test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+SRCDIR="$(cd "$(dirname "$0")" && pwd)"
+
+DIR="$(mktemp -d)"
+
+cd "$DIR"
+
+function cleanup {
+	rm -rf "$DIR"
+}
+
+trap cleanup EXIT
+
+$SRCDIR/generate_c_headers.py
+
+x86_64-unknown-cloudabi-cc -x c -c -fsyntax-only - <<EOF
+#include <stdint.h>
+#include <stddef.h>
+
+#include "syscalldefs-mi.h"
+#include "syscalldefs-md.h"
+#include "syscalldefs-md-32.h"
+#include "syscalldefs-md-64.h"
+
+#include "syscalls.h"
+#include "syscalls-x86_64.h"
+EOF
+
+aarch64-unknown-cloudabi-cc -x c -c -fsyntax-only - <<EOF
+#include <stdint.h>
+#include <stddef.h>
+
+#include "syscalldefs-mi.h"
+#include "syscalldefs-md.h"
+#include "syscalldefs-md-32.h"
+#include "syscalldefs-md-64.h"
+
+#include "syscalls.h"
+#include "syscalls-aarch64.h"
+EOF
+
+echo "Generated C header files compiled succesfully."
