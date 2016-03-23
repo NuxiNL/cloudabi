8166a7cd3d8baccfcdf862310d2ce10db50eb379
Maurice Bos <m-ou.se@m-ou.se>
2016-03-23 17:58:13 +0100

    Add syscalls.master generation.

---
+++ src/generators/syscalls_master.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2016 Nuxi, https://nuxi.nl/
+#
+# This file is distributed under a 2-clause BSD license.
+# See the LICENSE file for details.
+
+from ..abi import *
+from ..generator import *
+
+
+class SyscallsMasterGenerator(Generator):
+
+    def __init__(self, naming):
+        super().__init__(comment_prefix='; ')
+        self.naming = naming
+
+    def generate_abi(self, abi):
+        self.generate_head(abi)
+        self.generate_includes(abi)
+        self.generate_types(abi, abi.types)
+        self.generate_syscalls(abi, abi.syscalls_by_number)
+        self.generate_foot(abi)
+
+    def generate_head(self, abi):
+        print(' $FreeBSD$\n')
+        super().generate_head(abi)
+
+    def generate_includes(self, abi):
+        print('#include <sys/sysent.h>')
+        print('#include <sys/sysproto.h>')
+        print()
+        print('#include <compat/cloudabi64/cloudabi64_types.h>')
+        print('#include <compat/cloudabi64/cloudabi64_proto.h>')
+
+    def generate_types(self, abi, types):
+        pass
+
+    def generate_syscall(self, abi, syscall):
+        line_break = ' \\\n\t\t\t\t\t'
+
+        params = [line_break + self.naming.vardecl(p.type, p.name)
+                  for p in syscall.input.raw_members]
+
+        return_type = VoidType()
+        if len(syscall.output.raw_members) == 1:
+            t = syscall.output.raw_members[0].type
+            if not isinstance(t, PointerType):
+                return_type = t
+
+        return_type_name = self.naming.typename(return_type)
+        print('\n{}\t{}\t{}\t{{ {}{}{}({}); }}'.format(
+            syscall.number,
+            'AUE_NULL',
+            'STD',
+            return_type_name,
+            ' ' if len(return_type_name) < 16 else line_break,
+            self.naming.syscallname(syscall),
+            ','.join(params)))
