db2b56700a5950b4ac6034b5ba794aaeb93f1aeb
Maurice Bos <m-ou.se@m-ou.se>
2016-03-24 22:58:05 +0100

    Update test.sh.

--- test.sh
+++ test.sh
@@ -14,25 +14,20 @@ cleanup() {
 
 trap cleanup EXIT
 
-$SRCDIR/generate_c_headers.py
+mkdir headers freebsd docs
+
+$SRCDIR/generate.py
 
 for compiler in x86_64-unknown-cloudabi-cc aarch64-unknown-cloudabi-cc cc 'cc -m32'; do
 
-$compiler -x c -c -fsyntax-only - <<EOF
-#include <stdint.h>
-#include <stddef.h>
-
-#include "syscalldefs-mi.h"
-#include "syscalldefs-md.h"
-#include "syscalldefs-md-32.h"
-#include "syscalldefs-md-64.h"
-
-#include "syscalls.h"
-#ifdef __aarch64__
-#include "syscalls-aarch64.h"
-#endif
-#ifdef __x86_64__
-#include "syscalls-x86_64.h"
+$compiler -Iheaders -x c -c -fsyntax-only - <<EOF
+#include "cloudabi_types.h"
+#include "cloudabi32_types.h"
+#include "cloudabi64_types.h"
+#if defined(__aarch64__)
+#include "cloudabi_syscalls_aarch64.h"
+#elif defined(__x86_64__)
+#include "cloudabi_syscalls_x86_64.h"
 #endif
 EOF
 
